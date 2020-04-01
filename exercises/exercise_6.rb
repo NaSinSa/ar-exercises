193714bd8e1dcc489f19a635da36872858a4c6a8
NaSinSa <andy.k.oh@hotmail.com>
2020-04-01 02:03:54 +0000

    ex6 done and update store/employee models

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "abba", last_name: "bob", hourly_rate: 60)
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 20)
+@store1.employees.create(first_name: "dd", last_name: "bee", hourly_rate: 30)
+@store2.employees.create(first_name: "zaa", last_name: "dee", hourly_rate: 15)
+@store2.employees.create(first_name: "raa", last_name: "vee", hourly_rate: 22)
+@store2.employees.create(first_name: "wee", last_name: "xer", hourly_rate: 77)
